16b9a456f34f3f977f4504e16999d242cde19423
lbarbier22 <barbier.lucas08@gmail.com>
2026-07-28 12:02:57 +0200

    chore (#1): init spring boot project skeleton + datasource config

---
+++ src/main/java/com/productpark/datacom/DatacomApplication.java
@@ -0,0 +1,13 @@
+package com.productpark.datacom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatacomApplication.class, args);
+    }
+
+}
